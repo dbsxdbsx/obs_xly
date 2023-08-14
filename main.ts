09c957860079382abb7475550e2ab9562ba9942c
dbsxdbsx <djw0214@gmail.com>
2023-08-14 14:22:50 +0800

    add show note created/last modified time feature

--- main.ts
+++ main.ts
@@ -1,35 +1,74 @@
-import { App, /* Editor, MarkdownView, Modal, */ Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	/* Editor, MarkdownView, Modal, */ Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	WorkspaceLeaf,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	showNoteTime: boolean;
+	activateMacro: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	showNoteTime: false,
+	activateMacro: false,
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	showPopupMessage(message: string) {
+		new Notice(message, 3500);
+	}
+
 	async onload() {
 		await this.loadSettings();
+		const statusBarItemEl = this.addStatusBarItem(); // add status bar object
+		// const markdownView =
+		// 	this.app.workspace.getActiveViewOfType(MarkdownView);
+		// this.registerEvent(trigger_show_notes_time(statusBarItemEl));
+		this.registerEvent(
+			this.app.workspace.on(
+				"active-leaf-change",
+				async (leaf: WorkspaceLeaf) => {
+					trigger_show_notes_time(this, leaf, statusBarItemEl);
+				},
+			),
+		);
+
+		// this.app.workspace.on("codemirror", (cm: CodeMirror.Editor) => {
+		// 	const handler = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
+		// 		if (event.key === "w") {
+		// 			this.showPopupMessage("w is pressed");
+		// 		}
+		// 	};
+		// 	cm.on("keydown", handler);
+		// 	this.registerEvent(() => cm.off("keydown", handler));
+		// });
+
+		// this.app.workspace.on("codemirror", this.handleCodeMirror);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		// const ribbonIconEl = this.addRibbonIcon(
+		// 	"dice",
+		// 	"Sample Plugin",
+		// 	(evt: MouseEvent) => {
+		// 		// Called when the user clicks the icon.
+		// 		new Notice("This is a notice!");
+		// 	},
+		// );
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		// // This adds a simple command that can be triggered anywhere
 		// this.addCommand({
@@ -42,57 +81,79 @@ export default class MyPlugin extends Plugin {
 
 		// // This adds an editor command that can perform some operation on the current editor instance
 		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
+		// 	id: "sample-editor-command",
+		// 	name: "Sample editor command",
 		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
 		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
+		// 		editor.replaceSelection("Sample Editor Command");
+		// 	},
 		// });
-
-		// // This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
-
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// // Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
+		// this.app.workspace.iterateCodeMirrors((cm) => {
+		// 	cm.off("keydown", this.handleKeyPress);
+		// });
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 		console.log("Loaded settings");
-		new Notice('Loaded settings notice!');
+		this.showPopupMessage("Loaded settings notice!");
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	// handleCodeMirror = (cm: CodeMirror.Editor) => {
+	// 	this.registerEvent(() => cm.on("keydown", this.handleKeyPress));
+	// 	this.registerEvent(() => cm.off("keydown", this.handleKeyPress));
+	// };
+
+	// handleKeyPress = (cm: CodeMirror.Editor, event: KeyboardEvent) => {
+	// 	if (event.key === "w") {
+	// 		this.showPopupMessage("w is pressed");
+	// 	}
+	// };
+}
+
+export function trigger_show_notes_time(
+	plugin: MyPlugin,
+	leaf: WorkspaceLeaf,
+	statusBarItemEl: HTMLElement,
+) {
+	// plugin.showPopupMessage(
+	// 	`file inside! the flag is ${plugin.settings.showNoteTime}`,
+	// );
+	if (plugin.settings.showNoteTime) {
+		if (leaf.view.getViewType() === "markdown") {
+			const file = (leaf.view as MarkdownView).file;
+
+			if (file instanceof TFile) {
+				const created = window
+					.moment(file.stat.ctime)
+					.format("YYYY-MM-DD HH:mm:ss");
+				const lastModified = window
+					.moment(file.stat.mtime)
+					.format("YYYY-MM-DD HH:mm:ss");
+
+				const lang = window.localStorage.getItem("language");
+				// plugin.showPopupMessage(`the languge is ${lang}`)
+				const statusText =
+					lang === "zh"
+						? `最后修改时间：${lastModified} | 创建时间：${created}`
+						: `Last Modified: ${lastModified} | Created: ${created}`;
+
+				statusBarItemEl.setText(statusText);
+			}
+		}
+	} else {
+		statusBarItemEl.setText("");
 	}
 }
 
@@ -121,19 +182,39 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Show Notes Created/Modified Time")
+			.setDesc("Show notes created/last modified time in status bar")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.showNoteTime === true)
+					.onChange(async (value) => {
+						this.plugin.settings.showNoteTime = value;
+						// this.plugin.showPopupMessage(`the flag is ${this.plugin.settings.showNoteTime}`)
+						await this.plugin.saveSettings();
+					})
+					.setTooltip(
+						"Toggle to show/hide notes created/modified time in status bar",
+					),
+			);
+
+		// new Setting(containerEl)
+		// 	.setName("Activate Macro")
+		// 	.setDesc("activate some hot key in notes")
+		// 	.addToggle((toggle) =>
+		// 		toggle
+		// 			.setValue(this.plugin.settings.activateMacro === "on")
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.activateMacro = value
+		// 					? "on"
+		// 					: "off";
+		// 				await this.plugin.saveSettings();
+		// 			})
+		// 			.setTooltip("Toggle to activate/deactivate macro"),
+		// 	);
 	}
 }
